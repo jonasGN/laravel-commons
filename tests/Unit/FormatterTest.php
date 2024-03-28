f17a8cea108a3f703ac796079373b1fba99f14a6
jonasGN <j.goncalvesneto@outlook.com>
2024-03-28 08:41:05 -0300

    testes de exemplo removidos, novos testes adicionados e alguns testes corrigidos

--- tests/Unit/FormatterTest.php
+++ tests/Unit/FormatterTest.php
@@ -4,8 +4,12 @@ use Jonasgn\LaravelCommons\Formatter;
 
 describe('Method: toOnlyNumbers', function () {
     it('should not be empty', function (string $value) {
-        expect($value)->not->toBeEmpty();
-    })->with(['9090jjjj', 'uhaush9', 'aijs999asi']);
+        expect(Formatter::toOnlyNumbers($value))->not->toBeEmpty();
+    })->with(['m9obLJQhezVLS2', 'P^J!3pRs7Kbwuj']);
+
+    it('should be empty', function (string $value) {
+        expect(Formatter::toOnlyNumbers($value))->toBeEmpty();
+    })->with(['SyPcnfGHipXGBt', 'b^T@XpcrmW#xLT']);
 
     it('should be only numeric trimed chars', function () {
         expect(Formatter::toOnlyNumbers('ashuahs99888asa'))->toBe('99888');
@@ -19,12 +23,12 @@ describe('Method: toOnlyNumbers', function () {
 });
 
 describe('Method: toPhoneNumber', function () {
-    it('should throw when phone number is invalid', function () {
-        expect(fn () => Formatter::toPhoneNumber('9999'))->toThrow(
+    it('should throw when phone number is invalid', function (string $value) {
+        expect(fn () => Formatter::toPhoneNumber($value))->toThrow(
             InvalidArgumentException::class,
-            "O valor '9999', não pode ser transformado em número de telefone"
+            "O valor '{$value}', não pode ser transformado em número de telefone"
         );
-    });
+    })->with(['', '99999999', '999999999', 'hh9999']);
 
     it('should format phone number', function (string $value, string $result) {
         expect(Formatter::toPhoneNumber($value))->toBe($result);
@@ -34,4 +38,17 @@ describe('Method: toPhoneNumber', function () {
         ['3188887777', '(31) 8888-7777'],
         ['1177776666', '(11) 7777-6666'],
     ]);
+});
+
+describe('Method: toCep', function () {
+    it('should throw when cep number is not valid', function (string $value) {
+        expect(fn () => Formatter::toCep($value))->toThrow(
+            InvalidArgumentException::class,
+            "O valor '{$value}', não pode ser transformado em um CEP válido"
+        );
+    })->with(['3200000', '320000000']);
+
+    it('should be a valid formated cep number', function () {
+        expect(Formatter::toCep('32000000'))->toBe('32000-000');
+    });
 });
