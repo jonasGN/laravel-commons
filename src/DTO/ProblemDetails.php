8530479fcca6964851a7e3210ae920dea8756e28
JonasGN <j.goncalvesneto@outlook.com>
2024-04-02 18:08:38 -0300

    dtos de resposta atualizados

--- src/DTO/ProblemDetails.php
+++ src/DTO/ProblemDetails.php
@@ -2,10 +2,102 @@
 
 namespace Jonasgn\LaravelCommons\DTO;
 
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use JsonSerializable;
+
 /**
  * Resposta de erro padronizada de acordo com o padrão Problem Details conforme a RFC 7807
  * https://datatracker.ietf.org/doc/html/rfc9457
  */
-class ProblemDetails
+class ProblemDetails implements JsonSerializable
 {
+    private int $statusCode;
+    private string $title;
+    private string $details;
+    private string $instance;
+    private string $meta;
+
+    public function __construct(
+        string $title,
+        string $details,
+        string $instance,
+        int $statusCode,
+        array $meta = []
+    ) {
+        $this->title = $title;
+        $this->details = $details;
+        $this->instance = $instance;
+        $this->statusCode = $statusCode;
+        $this->meta = $meta;
+    }
+
+    public static function new(
+        string $title,
+        string $details,
+        Request $request,
+        int $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR,
+        array $meta = []
+    ): ProblemDetails {
+        return new ProblemDetails(
+            title: $title,
+            details: $details,
+            instance: $request->path(),
+            statusCode: $statusCode,
+            meta: $meta,
+        );
+    }
+
+    public function response(): Response
+    {
+        return response($this, $this->statusCode, [
+            'Content-Type' => 'application/problem+json',
+            'Content-Language' => 'en',
+        ]);
+    }
+
+    public function jsonSerialize(): mixed
+    {
+        return array_merge([
+            "type" => $this->_getTypeRef(),
+            "title" => $this->title,
+            "detail" => $this->details,
+            "instance" => $this->instance,
+        ], $this->meta);
+    }
+
+    private function _getTypeRef(): string
+    {
+        return match ($this->statusCode) {
+            400 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-400-bad-request',
+            401 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-401-unauthorized',
+            402 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-402-payment-required',
+            403 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-403-forbidden',
+            404 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-404-not-found',
+            405 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-405-method-not-allowed',
+            406 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-406-not-acceptable',
+            407 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-407-proxy-authentication-re',
+            408 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-408-request-timeout',
+            409 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-409-conflict',
+            410 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-410-gone',
+            411 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-411-length-required',
+            412 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-412-precondition-failed',
+            413 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-413-content-too-large',
+            414 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-414-uri-too-long',
+            415 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-415-unsupported-media-type',
+            416 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-416-range-not-satisfiable',
+            417 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-417-expectation-failed',
+            418 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-418-unused',
+            421 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-421-misdirected-request',
+            422 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-422-unprocessable-content',
+            426 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-426-upgrade-required',
+            500 => 'https://datatracker.ietf.org/doc/html/rfc9110#section-15.6.1',
+            501 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-501-not-implemented',
+            502 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-502-bad-gateway',
+            503 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-503-service-unavailable',
+            504 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-504-gateway-timeout',
+            505 => 'https://datatracker.ietf.org/doc/html/rfc9110#name-505-http-version-not-suppor',
+            default => 'https://datatracker.ietf.org/doc/html/rfc9110#name-status-codes',
+        };
+    }
 }
--- src/DTO/SuccessResponse.php
+++ src/DTO/SuccessResponse.php
@@ -2,112 +2,60 @@
 
 namespace Jonasgn\LaravelCommons\DTO;
 
-use Exception;
 use Illuminate\Http\Response;
+use JsonSerializable;
 
 /**
  * Resposta padronizada dos recursos da aplicação
  */
-class SuccessResponse
+class SuccessResponse implements JsonSerializable
 {
+    private mixed $data;
     private int $statusCode;
-    private ?Exception $exception;
+    private array $meta;
 
-    private function __construct(int $statusCode, ?Exception $exception = null)
+    private function __construct(mixed $data, int $statusCode)
     {
+        $this->data = $data;
         $this->statusCode = $statusCode;
-        $this->exception = $exception;
     }
 
     /**
      * Cria uma instância da classe a partir do construtor padrão.
      * Facilita a leitura do código
      */
-    public static function new(int $statusCode, ?Exception $exception = null): SuccessResponse
+    public static function new(mixed $data, int $statusCode = Response::HTTP_OK): SuccessResponse
     {
-        return new SuccessResponse($statusCode, $exception);
+        return new SuccessResponse($data, $statusCode);
     }
 
     /**
-     * Cria uma nova instância de response já configurando a resposta para NO_CONTENT
-     * @deprecated Utilize o método `noContent`
+     * Adiciona notas para os campos.
+     * Ideal para avisos sobre o determinado campo, bem como para sinalizar obsolescência.
      */
-    public static function withNoContent(): Response
+    public function addFieldNotes(array $meta): void
     {
-        return static::noContent();
+        $this->meta = ['field_notes' => $meta];
     }
 
     public static function noContent(): Response
     {
-        $dto = new SuccessResponse(Response::HTTP_NO_CONTENT);
-        return $dto->toResponse(null);
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 
     /**
      * Cria um objeto de resposta HTTP padronizado, a partir dessa instância, para ser utilizado nas
      * respostas bem sucedidas da aplicação
      */
-    public function toResponse(mixed $data): Response
+    public function jsonSerialize(): mixed
     {
-        $publicResponse = $this->_getDefaultResponseObject([
-            'data' => $data,
-            'error' => null,
-        ]);
-        $debugResponse = $this->_getDeveloperErrorDetails();
-
-        return response(array_merge($publicResponse, $debugResponse), $this->statusCode);
-    }
-
-    /**
-     * Método auxiliar para lidar com união de dados via array
-     */
-    public function toResponseWithMerge(...$data): Response
-    {
-        return $this->toResponse(array_merge(...$data));
-    }
-
-    /**
-     * Cria um objeto de resposta HTTP padronizado, a partir dessa instância, para ser utilizado nos
-     * erros da aplicação
-     */
-    public function toErrorResponse(string $cause, string $message): Response
-    {
-        $publicResponse = $this->_getDefaultResponseObject([
-            'data' => null,
-            'error' => [
-                'cause' => $cause,
-                'message' => $message,
-            ]
-        ]);
-        $debugResponse = $this->_getDeveloperErrorDetails();
-
-        return response(array_merge($publicResponse, $debugResponse), $this->statusCode);
-    }
-
-    /**
-     * Retorna um objeto padrão, tanto para sucesso quanto para erros da aplicação
-     */
-    private function _getDefaultResponseObject(array $values): array
-    {
-        return array_merge([
-            'status_code' => $this->statusCode,
-            'moment' => now(),
-        ], $values);
-    }
-
-    /**
-     * Mostra os detalhes do erro apenas em modo de desenvolvimento.
-     * Isso evita com que informações sensíveis sejam vazadas para o client em produção
-     */
-    private function _getDeveloperErrorDetails(): array
-    {
-        if (!app()->isLocal() || $this->exception == null) return [];
-
-        return [
-            'internal_exception_name' => $this->exception::class,
-            'internal_code' => $this->exception->getCode(),
-            'internal_message' => $this->exception->getMessage(),
-            'internal_trace' => $this->exception->getTrace(),
-        ];
+        return array_merge(
+            [
+                'status_code' => $this->statusCode,
+                'moment' => now(),
+                'data' => $this->data,
+            ],
+            isset($this->meta) ? ['meta' => $this->meta] : null
+        );
     }
 }
