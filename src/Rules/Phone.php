7bdd2cff3fdf54ddda7fba1b2451a1a25ed6c737
jonasGN <j.goncalvesneto@outlook.com>
2024-12-10 14:44:36 -0300

    regra de validação de telefone corrigida

--- src/Rules/Phone.php
+++ src/Rules/Phone.php
@@ -8,6 +8,77 @@ use Jonasgn\LaravelCommons\Formatter;
 
 class Phone implements ValidationRule
 {
+    // todos os DDDS do Brasil
+    private const DDDS = [
+        11, // São Paulo
+        12, // São Paulo
+        13, // São Paulo
+        14, // São Paulo
+        15, // São Paulo
+        16, // São Paulo
+        17, // São Paulo
+        18, // São Paulo
+        19, // São Paulo
+        21, // Rio de Janeiro
+        22, // Rio de Janeiro
+        24, // Rio de Janeiro
+        27, // Espirito Santo
+        28, // Espirito Santo
+        31, // Minas Gerais
+        32, // Minas Gerais
+        33, // Minas Gerais
+        34, // Minas Gerais
+        35, // Minas Gerais
+        37, // Minas Gerais
+        38, // Minas Gerais
+        41, // Paraná
+        42, // Paraná
+        43, // Paraná
+        44, // Paraná
+        45, // Paraná
+        46, // Paraná
+        47, // Santa Catarina
+        48, // Santa Catarina
+        49, // Santa Catarina
+        51, // Rio Grande do Sul
+        53, //– Rio Grande do Sul
+        54, //– Rio Grande do Sul
+        55, //– Rio Grande do Sul
+        61, // Goiás e Distrito Federal
+        62, // Goiás
+        63, // Tocantis
+        64, // Goiás
+        65, // Mato Grosso
+        66, // Mato Grosso
+        67, // Mato Grosso do Sul
+        68, // Acre
+        69, // Rondônia
+        71, // Salvador
+        73, // Salvador
+        74, // Salvador
+        75, // Salvador
+        77, // Salvador
+        79, // Sergipe
+        81, // Pernambuco
+        82, // Alagoas
+        83, // Paraíba
+        84, // Rio Grande do Norte
+        85, // Ceará
+        86, // Piauí
+        87, // Pernambuco
+        88, // Ceará
+        89, // Piauí
+        91, // Pará
+        92, // Amazonas
+        93, // Pará
+        94, // Pará
+        95, // Roraima
+        96, // Amapá
+        97, // Amazonas
+        98, // Maranhão
+        99, // Maranhão
+    ];
+
     /**
      * Run the validation rule.
      *
@@ -15,24 +86,33 @@ class Phone implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        // verifica se o número de telefone está em um dos formatos: `(00) 98888-7777` ou `(00) 8888-9999`
-        $hasPhonePattern = preg_match('/^\(\d{2}\)\s?\d{4,5}-\d{4}$/', $value) > 0;
+        $value = Formatter::toOnlyNumbers($value);
+        $defaultErrorMessage = "O campo :attribute, não é um número de telefone válido";
 
-        // se o pattern estiver correto, significa que pode ser um número de telefone válido
-        if ($hasPhonePattern) return;
-
-        $failMessage = 'O campo :attribute, não é um número de telefone com DDD válido';
-
-        $hasOnlyNumbers = Formatter::hasOnlyNumbers($value);
-        if (!$hasOnlyNumbers) {
-            $fail($failMessage);
+        $length = strlen($value);
+        // verifica se contém 10 ou 11 digitos
+        if ($length < 10 || $length > 11) {
+            $fail($defaultErrorMessage);
             return;
         }
 
-        $phoneLenght = strlen(Formatter::toOnlyNumbers($value));
-        $hasPhoneLength = $phoneLenght === 10 || $phoneLenght === 11;
-        if (!$hasPhoneLength) {
-            $fail($failMessage);
+        // verifica se todos os números são iguais
+        if (count(array_filter(count_chars(substr($value, 2), 1))) === 1) {
+            $fail($defaultErrorMessage);
+            return;
+        }
+
+        // verifica se o DDD é válido
+        $ddd = (int) substr($value, 0, 2);
+        if (!in_array($ddd, $this::DDDS)) {
+            $fail('O campo :attribute, não possui um número de DDD válido');
+            return;
+        }
+
+        // verifica o 9° digito, quando celular
+        if ($length === 11 && (int) substr($value, 2, 1) !== 9) {
+            $fail($defaultErrorMessage);
+            return;
         }
     }
 }
